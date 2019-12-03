48485149e1c6a17af4bb8a47ba3996dbe012d576
Nick Corona <coronaisme@gmail.com>
2019-12-03 21:14:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,5 +26,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross_total = 0 
+  index = 0 
+  
+  while index < director_data
   
 end
