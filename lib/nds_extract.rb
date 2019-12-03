02e4396de9e8ce5fa4f7137bae8944f1c65e71d5
Nick Corona <coronaisme@gmail.com>
2019-12-03 21:12:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,7 +16,9 @@ def directors_totals(nds)
     
     while movie_index < nds[director_index][:movies].length do
       result[director_name] += nds[director_index][:movies][:movie_index][:worldwide_gross]
-      
+      movie_index += 1 
+    end
+    director_index += 1
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
