fd73d2d6013690db7cd5a18a08677d890346183e
Nick Corona <coronaisme@gmail.com>
2019-12-03 21:12:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,6 +19,8 @@ def directors_totals(nds)
       movie_index += 1 
     end
     director_index += 1
+  end
+  result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
